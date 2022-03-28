0f13c69160451e72b9558fd723d15858d6f9ec7f
laevatein2070 <zenzoza007@gail.com>
2022-03-28 15:55:15 +0700

    add critical and machine gun mechanics

--- pages/threes.tsx
+++ pages/threes.tsx
@@ -1,56 +1,110 @@
-import { ApiError } from "next/dist/server/api-utils";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useEffect, useRef, useState } from "react";
-import Countdown, { CountdownApi } from 'react-countdown';
+import React, { useEffect, useState } from "react";
+import Countdown from 'react-countdown';
 
 // styles
 const StartButton = 'text-4xl font-semibold bg-white border border-white rounded-md text-black py-12 px-10 transition duration-300 hover:text-white hover:bg-slate-900 hover:-translate-y-6 hover:border-yellow-300 active:scale-90';
-const Button = 'text-4xl font-semibold bg-white rounded-md text-black py-12 px-8 transition duration-300 active:duration-100 hover:-translate-y-2 hover:text-blue-600 active:scale-90';
+const Button = 'text-4xl font-semibold bg-white rounded-md text-black py-12 transition duration-300 active:duration-100 hover:-translate-y-2 hover:text-blue-600 active:scale-90';
+const ButtonCritical = 'text-4xl font-semibold bg-red-500 text-white rounded-md py-12 animate-pulse transition duration-300 active:duration-100 hover:-translate-y-2 hover:text-yellow-200 active:scale-110';
+const ButtonMachine = 'text-4xl font-semibold bg-green-500 text-white rounded-md py-12 animate-pulse transition duration-300 active:duration-100 hover:-translate-y-2 hover:text-yellow-200 active:scale-110';
 
 const Threes = () => {
 
+  const [machine_count, setMachineCount] = useState(0);
   const [time, setTime] = useState(Date.now() + 59000);
   const [start, setStart] = useState(false);
+  const [finish, setFinish] = useState(false);
   const [count, setCount] = useState(0);
+  const [high, setHigh] = useState(0);
+  const [score, setHighscore] = useState(0);
+  const [highnum, setHighNumber] = useState(0);
 
   const [numbers, setNumber] = useState([
-    { id: 0, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10 },
-    { id: 1, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10 },
-    { id: 2, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10 },
+    { id: 0, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
+    { id: 1, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
+    { id: 2, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
   ]);
+  const init = [
+    { id: 0, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
+    { id: 1, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
+    { id: 2, value: Math.floor(Math.random() * (99 - 10 + 1) ) + 10, crit: false, machine: false },
+  ];
 
   useEffect(() => {
-    if (start) setTime(Date.now() + 60000);
+    if (start) setTime(Date.now() + 59000);
   }, [start]);
 
   const handleStart = (e: any) => {
+    setNumber(init);
     setTimeout(() => setStart(!start), 300);
+    setCount(0);
+    setFinish(false);
   }
 
   const handleFinished = () => {
+    if (count > score) {
+      setHighscore(count);
+    }
+    if (high > highnum) {
+      setHighNumber(high);
+    }
     setStart(false);
+    setFinish(true);
   }
 
-  const handleSelect = (id: number, val: number) => {
-    const select = numbers[id].value;
+  const handleSelect = (id: number, select: number, critical: boolean, machine: boolean) => {
     const min = numbers.reduce(
       (num1, num2) =>
         num1.value < num2.value ? num1 : num2
     )
 
-    if (select == min.value) {
+    if (critical) {
+      setCount(count + 10);
+      let newNumbers = [...numbers];
+      newNumbers[0].value = (count * count) + select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
+      newNumbers[1].value = (count * count) + select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
+      newNumbers[2].value = (count * count) + select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
+      setNumber(newNumbers);
+      setHigh(select);
+      newNumbers[id].crit = false;
+    }
+    else if (machine) {
       setCount(count + 1);
       let newNumbers = [...numbers];
-      newNumbers[id].value = select + Math.floor(Math.random() * (99 - 10 + 1) ) + 10;
+      newNumbers[0].value = select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
+      newNumbers[1].value = select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
+      newNumbers[2].value = select + Math.floor(Math.random() * (count + 99 - 10 + 1) ) + 10;
       setNumber(newNumbers);
+      setHigh(select);
+      if (machine_count == 10) {
+        newNumbers[id].machine = false;
+        setMachineCount(0);
+      }
+      else {
+        setMachineCount(machine_count + 1);
+      }
+    }
+    else if (select == min.value) {
+      setCount(count + 1);
+      let newNumbers = [...numbers];
+      let crit = Math.floor(Math.random() * (100 - 10 + 1) ) + 10;
+      let machine = Math.floor(Math.random() * (100 - 10 + 1) ) + 10;
+      newNumbers[id].value = count + select + Math.floor(Math.random() * (99 - 10 + 1) ) + 10;
+      if (crit % 10 == 0) {
+        newNumbers[id].crit = true;
+      }
+      else if (machine % 10 == 0) {
+        newNumbers[id].machine = true;
+      }
+      setNumber(newNumbers);
+      setHigh(select);
     }
     else {
       count - 5 > 0 ? setCount(count - 5) : setCount(0);
     }
   }
 
-  // Renderer callback with condition
   const renderer = ({ seconds, completed}: any) => {
     if (completed) {
       return <span>0</span>;
@@ -60,7 +114,7 @@ const Threes = () => {
   };
 
   return (
-    <div className="bg-slate-900">
+    <div className="bg-slate-900 h-screen">
       <Head>
         <title>THR3ES</title>
         <link rel="icon" href="/favicon.ico" />
@@ -74,8 +128,8 @@ const Threes = () => {
         </Link>
       </header>
 
-      <main className="place h-screen">
-        <h1 className="text-9xl p-24 text-white text-center">
+      <main>
+        <h1 className="text-9xl p-20 text-white text-center">
           {start ?
           <Countdown
             key={time}
@@ -91,11 +145,11 @@ const Threes = () => {
           { !start?
             <button onClick={handleStart} className={StartButton}>Start</button>
             :
-            numbers.map(({id, value}) => (
+            numbers.map(({id, value, crit, machine}) => (
               <button 
                 key={id} 
-                className={Button} 
-                onClick={() => handleSelect(id, value)}
+                className={crit ? ButtonCritical : machine ? ButtonMachine : Button} 
+                onClick={() => handleSelect(id, value, crit, machine)}
                 style={{maxWidth: 120, minWidth:120}}
               >
                 {value}
@@ -103,11 +157,14 @@ const Threes = () => {
             ))
           }
         </div>
-        <h1 className="text-xl text-center pt-20 text-white">
-          Score: {count}
+        <h1 className="text-xl text-center pt-16 text-white">
+          {finish ? `Well Done! You got ${count} point(s)!` : `Score: ${count}`}
+        </h1>
+        <h1 className="text-xl text-center pt-4 text-white">
+          High Score: {score} | Highest Number: {highnum}
         </h1>
         <h1 className="text-xl text-center pt-8 text-white">
-          Select a number from smallest to largest. You have 60 seconds each round. Gotta go fast!
+          Select smallest number from the group. You have 60 seconds each round. Gotta go fast!
         </h1>
       </main>
     </div>
